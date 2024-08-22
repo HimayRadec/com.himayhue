51407afc460b66f8b65f39149033bd3341b2aa8a
HimayRadec <80723658+HimayRadec@users.noreply.github.com>
2024-08-22 00:18:38 -0700

    Reformatted Maps Code

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -3,26 +3,44 @@ import React, { useEffect, useRef } from 'react';
 import { Loader } from "@googlemaps/js-api-loader";
 
 export default function Map() {
-   const mapRef = useRef(null);
+   const mapRef = useRef<HTMLDivElement>(null);
 
    useEffect(() => {
-      const apiKey: string = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || "";
-
       const loader = new Loader({
-         apiKey: apiKey, // Replace with your actual Google Maps API key
-         version: "weekly",
+         apiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string,
+         version: "weekly", // Ensure you're using the latest version
       });
 
-      loader.load().then(() => {
-         if (mapRef.current) {
-            const map = new google.maps.Map(mapRef.current, {
-               center: { lat: 33.4484, lng: -112.0740 }, // Phoenix coordinates
-               zoom: 10,
-            });
-         }
-      }).catch((e) => {
-         console.error("Error loading Google Maps", e);
-      });
+      const initMap = async () => {
+         if (!mapRef.current) return;
+
+         // Import the necessary libraries
+         const { Map } = await loader.importLibrary("maps");
+         const { AdvancedMarkerElement, PinElement } = await loader.importLibrary("marker");
+
+         const position = {
+            lat: 33.4484,
+            lng: -112.0740,
+         };
+
+         const mapOptions: google.maps.MapOptions = {
+            center: position,
+            zoom: 10,
+            mapId: "a1079c9cea2794a7",
+         };
+
+         // Create the map
+         const map = new Map(mapRef.current as HTMLDivElement, mapOptions);
+
+         // Create an advanced marker
+         new AdvancedMarkerElement({
+            map: map,
+            position: position,
+            title: "Title text for the marker at lat: 37.419, lng: -122.03",
+         });
+      };
+
+      initMap();
    }, []);
 
    return (
