0dbc39430afe40ab774f9cdaba485f258476597a
HimayRadec <80723658+HimayRadec@users.noreply.github.com>
2025-04-27 14:31:08 -0700

    Delete middleware.ts

--- src/middleware.ts
+++
@@ -1,6 +0,0 @@
-// middleware.ts
-import NextAuth from "next-auth";
-import authConfig from "./auth.config";
-
-// Simply export auth middleware directly
-export const { auth: middleware } = NextAuth(authConfig);
